8be287edb0895a6c2f073c351385fea8c73dea5d
Your Name <you@example.com>
2025-02-06 14:53:35 +0200

    Answer Q1

--- Assignment02_Part02_Advanced/Program.cs
+++ Assignment02_Part02_Advanced/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 1.Given a Queue, implement a function to reverse the elements of a queue using a stack.
+            
+            //Queue<int> queue = new Queue<int>();
+
+            //queue.Enqueue(1);
+            //queue.Enqueue(2);
+            //queue.Enqueue(3);
+            //queue.Enqueue(4);
+            //Console.WriteLine($"Queue Capacity:{queue.Capacity} Queue Count:{queue.Count}");
+            //foreach (var item in queue)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+
+            //Stack<int> stack = new Stack<int>();
+
+            //while (queue.Count > 0)
+            //{
+            //    stack.Push(queue.Dequeue());
+            //}
+
+            //Console.WriteLine($"\n\nStack Capacity:{stack.Capacity} Stack Count:{stack.Count}");
+            //Console.WriteLine("After Reverse Using Stack");
+
+            //foreach (var item in stack)
+            //{
+            //    Console.Write($"{item} ");
+            //} 
+
+            #endregion
+
+
         }
     }
 }
