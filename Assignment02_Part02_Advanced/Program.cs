4a43fea3a4ec799837fcb2db8397c18dbd750840
Your Name <you@example.com>
2025-02-06 14:34:57 +0200

    Add project files.

---
+++ Assignment02_Part02_Advanced/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment02_Part02_Advanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
